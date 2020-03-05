3d7d2ca695bb8461c7dbd72f7451904cad6d4534
Farzad Daud <daud.farzad1@gmail.com>
2020-03-05 01:06:02 +0000

    Automatically backed up by Learn

--- lib/directors_database.rb
+++ lib/directors_database.rb
@@ -1,6 +1,29 @@
-require 'yaml'
+def list_of_directors(source)
+    names = []
+   i = 0
 
-def directors_database
-	rot13 = -> (s) { s.tr('A-Za-z', 'N-ZA-Mn-za-m') }
-  @_db ||= YAML.load(rot13.call(File.open("directors_db").read.to_s))
-end
+   while i < source.length do
+     names << source[i][:name]
+     i += 1
+   end
+
+   names
+ end
+ 
+ def total_gross(source)
+   list_of_directors(source).reduce(0){|total, name| total +=  directors_totals(source)[name]}
+   dir_to_earnings_hash = directors_totals(source)
+   dir_names = list_of_directors(source)
+   i = 0
+
+   total = 0
+
+   while i < dir_names.length do
+     dir_name = dir_names[i]
+     total += dir_to_earnings_hash[dir_name]
+     i += 1
+   end
+
+   total
+ end
+ 
